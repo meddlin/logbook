f25c7f01b58ecd7dce2d711d030be2f14aeeea2c
meddlin <drushing.dev@gmail.com>
2019-03-20 00:12:54 -0500

    App properly sends data to API, sends JWT token back to API
    - Log component handles form via state with onChange handler
    - Removed "sayHello" placeholder from Log component
    - Added action, service for logs
    - Referenced new files in proper index.js files
    - Manually adding auth headers without 'authHeader()' ; need to fix that

---
+++ client/src/_services/log.service.js
@@ -0,0 +1,41 @@
+import { authHeader } from '../_helpers';
+
+export const logService = {
+	createLog
+};
+
+const config = {
+	apiUrl: 'https://localhost:5001'
+};
+
+
+function createLog(fuelLog) {
+	let user = JSON.parse(localStorage.getItem('user'));
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Authorization': `Bearer ${user.token}`, 'Content-Type': 'application/json' },
+        /*headers: new Headers(authHeader()).append('Content-Type', 'application/json'),*/
+        body: JSON.stringify(fuelLog)
+    };
+
+    return fetch(`${config.apiUrl}/api/Log/CreateLog`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                /*logout();*/
+                Location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
